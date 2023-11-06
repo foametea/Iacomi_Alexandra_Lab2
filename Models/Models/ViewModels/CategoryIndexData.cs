e708d77d9c8d4590efebee4d5cc78bddcdd253b9
Alexandra <asihara3@yahoo.com>
2023-11-06 14:59:28 +0200

    Adaugare la cateogies select

---
+++ Models/Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Iacomi_Alexandra_Lab2.Models.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,25 +7,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Iacomi_Alexandra_Lab2.Data;
 using Iacomi_Alexandra_Lab2.Models;
+using Iacomi_Alexandra_Lab2.Models.ViewModels;
 
 namespace Iacomi_Alexandra_Lab2.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-        private readonly Iacomi_Alexandra_Lab2.Data.Iacomi_Alexandra_Lab2Context _context;
+        private readonly Iacomi_Alexandra_Lab2Context _context;
 
-        public IndexModel(Iacomi_Alexandra_Lab2.Data.Iacomi_Alexandra_Lab2Context context)
+        public IndexModel(Iacomi_Alexandra_Lab2Context context)
         {
             _context = context;
         }
+        public IList<Category> Category { get; set; } = new List<Category>();
+        public List<Book> BooksInCategory { get; set; }
+        public int CategoryID { get; set; }
 
-        public IList<Category> Category { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id)
         {
-            if (_context.Category != null)
+            Category = await _context.Category
+                .Include(c => c.BookCategories)
+                .ThenInclude(bc => bc.Book)
+                .ThenInclude(b => b.Author)
+                .AsNoTracking()
+                .ToListAsync();
+
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+                Category selectedCategory = Category
+                    .Where(c => c.ID == id.Value)
+                    .SingleOrDefault();
+
+                if (selectedCategory != null)
+                {
+                    BooksInCategory = selectedCategory.BookCategories
+                        .Select(bc => bc.Book)
+                        .ToList();
+                }
             }
         }
     }
